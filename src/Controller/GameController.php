ffaa7b5ec6391074d396b72b488acfe728346ae3
Salicorne <timothee.Raboeuf@gmail.com>
2018-09-16 21:47:07 +0300

    Added filters to forbid users to interact with other editions (esp. towards Games)

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -23,23 +23,29 @@ class GameController extends CustomController {
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $game->setAuthor($this->getUser());
-
             $user = $this->getUser();
+            $game->setAuthor($user);
+
+            // Check if the slot belongs to the current edition
+            if($game->getGameSlot()->getEdition() != $this->getCurrentEdition()) {
+                $this->addFlash('danger', "Vous ne pouvez proposer une partie que pour l'édition actuelle !");
+                return $this->redirectToRoute('nouvellePartie');
+            }
+
             // Check if the user has no other game on the same slot
             $otherGames = $user->getPartiesJouees();
             foreach ($otherGames as $g) {
                 if($g->getGameSlot() == $game->getGameSlot()) {
                     $this->addFlash('danger', "Vous avez déjà la partie ".$g->getTitle()." prévue sur cet horaire !");
-                    return $this->redirectToRoute('showGame', ["id" => $id]);
+                    return $this->redirectToRoute('nouvellePartie');
                 }
             }
-            // Check if the user has not proposed an other game on this spot
+            // Check if the user has not proposed an other game on this slot
             $proposedGames = $user->getPartiesOrganisees();
             foreach ($proposedGames as $g) {
                 if($g->getGameSlot() == $game->getGameSlot()) {
                     $this->addFlash('danger', "Vous êtes déjà Maître du Jeu de la partie ".$g->getTitle()." sur cet horaire !");
-                    return $this->redirectToRoute('showGame', ["id" => $id]);
+                    return $this->redirectToRoute('nouvellePartie');
                 }
             }
 
@@ -47,6 +53,7 @@ class GameController extends CustomController {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($game);
             $entityManager->flush();
+            $this->addFlash('info', "Votre partie a bien été enregistrée ! Elle va être validée par notre équipe avant d'être mise en ligne. Merci pour votre investissement auprès du Festival !");
 
             return $this->redirectToRoute('listeParties');
         }
@@ -75,17 +82,17 @@ class GameController extends CustomController {
     public function listGames() {
         $games = $this->getDoctrine()->getRepository(Game::class)->findBy(["validated" => true]);
         $games = array_filter($games, function($element) {
-            return $element->getGameSlot()->getEdition()->getAnnee() == $this->getParameter('current_edition');
+            return $element->getGameSlot()->getEdition() == $this->getCurrentEdition();
         });
         
         $user = $this->getUser();
         $userGames = ($user != null) ? $this->getUser()->getPartiesJouees()->toArray() : array();
         $userGames = array_filter($userGames, function($element) {
-            return $element->getGameSlot()->getEdition()->getAnnee() == $this->getParameter('current_edition');
+            return $element->getGameSlot()->getEdition() == $this->getCurrentEdition();
         });
         $userProposedGames = ($user != null) ? $this->getUser()->getPartiesOrganisees()->toArray() : array();
         $userProposedGames = array_filter($userProposedGames, function($element) {
-            return $element->getGameSlot()->getEdition()->getAnnee() == $this->getParameter('current_edition');
+            return $element->getGameSlot()->getEdition() == $this->getCurrentEdition();
         });
 
         return $this->render('oeilglauque/gamesList.html.twig', array(
@@ -104,6 +111,13 @@ class GameController extends CustomController {
     public function showGame($id) {
         $game = $this->getDoctrine()->getRepository(Game::class)->find($id);
         if($game) {
+            $user = $this->getUser();
+            // Only the author can review a game that has not yet been validated
+            if(!$game->getValidated() && ($user == null || ($user != null && $game->getAuthor() != $user))) {
+                $this->addFlash('danger', "Cette partie n'a pas encore été validée par notre équipe. ");
+                return $this->redirectToRoute('listeParties');
+            }
+
             return $this->render('oeilglauque/showGame.html.twig', array(
                 'dates' => $this->getCurrentEdition()->getDates(), 
                 'game' => $game, 
@@ -121,6 +135,16 @@ class GameController extends CustomController {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         $game = $this->getDoctrine()->getRepository(Game::class)->find($id);
         if($game) {
+            // Check if the game is validated
+            if(!$game->getValidated()) {
+                $this->addFlash('danger', "Vous ne pouvez pas vous inscrire à une partie non validée !");
+                return $this->redirectToRoute('showGame', ["id" => $id]);
+            }
+            // Check if the game belongs to the current edition
+            if($game->getGameSlot()->getEdition() != $this->getCurrentEdition()) {
+                $this->addFlash('danger', "Vous ne pouvez vous inscrire qu'aux parties de l'édition actuelle !");
+                return $this->redirectToRoute('showGame', ["id" => $id]);
+            }
             $user = $this->getUser();
             // Check if the user has no other game on the same slot
             $otherGames = $user->getPartiesJouees();
