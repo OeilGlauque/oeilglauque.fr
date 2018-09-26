546ee3d629138a556dada705b4799d1594b38af2
Salicorne <timothee.Raboeuf@gmail.com>
2018-09-26 13:15:26 +0300

    Changed title markdown to end at the end of a line instead of a newline

--- src/Service/GlauqueMarkdownParser.php
+++ src/Service/GlauqueMarkdownParser.php
@@ -8,7 +8,7 @@ class GlauqueMarkdownParser
         $text = preg_replace('/\*\*(.*?)\*\*/', "<b>$1</b>", $text);
         $text = preg_replace('/\*(.*?)\*/', "<em>$1</em>", $text);
         $text = preg_replace('/__(.*?)__/', "<u>$1</u>", $text);
-        $text = preg_replace('/#(.*?)\n/', "<h3>$1</h3>\n", $text);
+        $text = preg_replace('/#(.*?)$/m', "<h3>$1</h3>\n", $text);
         $text = str_replace("\n", "<br />", $text);
         return $text;
     }
