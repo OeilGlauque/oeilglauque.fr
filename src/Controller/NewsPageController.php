358ebb3cf29ce2a1392fdaed76dd74a3c9e1a752
Antonin Gontier <gontier.antonin@orange.fr>
2021-04-26 16:53:36 +0200

    Add news to feature system

--- src/Controller/NewsPageController.php
+++ src/Controller/NewsPageController.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use App\Entity\News;
+use App\Entity\Feature;
 use App\Service\GlauqueMarkdownParser;
 
 class NewsPageController extends FOGController {
@@ -14,6 +15,10 @@ class NewsPageController extends FOGController {
      * @Route("/news", name="newsIndex")
      */
     public function index() {
+        if (!$this->getDoctrine()->getRepository(Feature::class)->find(6)->getState()) {
+            return $this->redirectToRoute('index');
+        }
+
         $news = $this->getDoctrine()->getRepository(News::class)->findAll();
         
         foreach ($news as $n) {
@@ -26,9 +31,13 @@ class NewsPageController extends FOGController {
     }
 
     /**
-     * ("/news/{slug}")
+     * @Route("/news/{slug}")
      */
     public function showNews($slug) {
+        if (!$this->getDoctrine()->getRepository(Feature::class)->find(6)->getState()) {
+            return $this->redirectToRoute('index');
+        }
+
         $news = $this->getDoctrine()->getRepository(News::class)->findOneBy(['slug' => $slug]);
         if($news) {
             $news->setText(GlauqueMarkdownParser::parse($news->getText()));
