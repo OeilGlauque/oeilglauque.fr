401f622cc3f17379b3caba5bba5c5c198ebd71d3
Antonin Gontier <agontier@hellowork.com>
2021-08-18 11:23:26 +0200

    fix game selection script

--- public/js/selectGames.js
+++ public/js/selectGames.js
@@ -1,12 +1,12 @@
 let slots = [];
 Array.from(document.getElementsByClassName('slots')).forEach(inp => {
-    slots[inp.getAttribute('data')] = {status: true, uptodate: false};
+    slots[inp.getAttribute('data')] = {id: parseInt(inp.getAttribute('data'), 10), status: true, uptodate: false};
 });
 
 Array.from(document.getElementsByClassName('btn-filtre')).forEach(button => {
     button.querySelector('input').onchange = (e) => {
-        const idx = e.target.getAttribute('data');
-        slots[idx] = { status: slots[idx].status, uptodate: slots[idx].status === e.target.checked };
+        const idx = parseInt(e.target.getAttribute('data'));
+        slots[idx] = { id: idx, status: slots[idx].status, uptodate: slots[idx].status === e.target.checked };
         if (e.target.checked) {
             button.removeAttribute('inactive');
         } else {
@@ -17,16 +17,16 @@ Array.from(document.getElementsByClassName('btn-filtre')).forEach(button => {
 });
 
 function updateGames() {
-    for(let i = 1; i < slots.length; i++) {
-        const gid = document.getElementsByClassName('ghid_' + i);
-        if (!slots[i].uptodate) {
-            if (slots[i].status) {
+    for(slotid in slots) {
+        const gid = document.getElementsByClassName('ghid_' + slotid);
+        if (!slots[slotid].uptodate) {
+            if (slots[slotid].status) {
                 Array.from(gid).forEach(g => {$(g).slideUp()});
             } else {
                 Array.from(gid).forEach(g => $(g).show("slow"));
             }
-            slots[i].status = !slots[i].status;
-            slots[i].uptodate = true;
+            slots[slotid].status = !slots[slotid].status;
+            slots[slotid].uptodate = true;
         }
     }
 }
@@ -39,14 +39,14 @@ toggleAll.querySelector('input').onchange= (e) => {
             button.querySelector('input').checked = true;
             button.removeAttribute('inactive');
         });
-        slots = slots.map(() => {return {status: false, uptodate: false}});
+        slots = slots.map(slot => {return {id: slot.id, status: false, uptodate: false}});
     } else {
         toggleAll.querySelector('span').innerText="Tout selectionner";
         Array.from(document.getElementsByClassName('btn-filtre')).forEach(button => {
             button.querySelector('input').checked = false;
             button.setAttribute('inactive', true);
         });
-        slots = slots.map(() => {return {status: true, uptodate: false}});
+        slots = slots.map(slot => {return {id: slot.id, status: true, uptodate: false}});
     }
     updateGames();
 };
