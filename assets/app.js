d16a2e52d71c7d92cbf36b0dc13383431570265c
ElAurelien <aurelienfoucher@icloud.com>
2025-03-16 11:40:28 +0100

    Admin reservation css

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,2 @@
 import './bootstrap.js';
-
 import './styles/app.css';
-
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉')
--- assets/controllers/hello_controller.js
+++ assets/controllers/hello_controller.js
@@ -10,7 +10,4 @@ import { Controller } from '@hotwired/stimulus';
  * Delete this file or adapt it for your use!
  */
 export default class extends Controller {
-    connect() {
-        this.element.textContent = 'Hello Stimulus! Edit me in assets/controllers/hello_controller.js';
-    }
 }
