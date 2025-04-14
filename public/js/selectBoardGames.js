2670da851443cde3a71266809af579a8b09395c1
ElAurelien <aurelienfoucher@icloud.com>
2025-04-14 19:22:01 +0200

    show more boardGames details

--- public/js/selectBoardGames.js
+++ public/js/selectBoardGames.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
                 games[elements[0]] = parseInt(elements[3]);
 
-                return `<div><span class="title">${escape(elements[0])}<span class="state ${escape(elements[2])}"> (${escape(elements[1])})</span></span><span class="price"> Caution: ${escape(elements[3])}€</span></div>`;
+                return `<div><span class="title">${escape(elements[0])}<span class="state ${escape(elements[2])}"> (${escape(elements[1])})</span></span><div class="secondary-container"><span class="secondary-el"> Caution: ${escape(elements[3])}€</span><span class="secondary-el">${escape(elements[5]) !== "" ? '🕺 ' + escape(elements[5]) + ' joueurs' : ""}</span><span class="secondary-el">${escape(elements[5]) !== "" ? '🕦 ' + escape(elements[4]) : ""}</span></div></div>`;
             },
             item: function (data, escape) {
                 return `<div>${escape(data.id)}<a onclick="onDeleteItem('${escape(data.id)}')" style="border-left: 1px solid #dee2e6; margin-left: 5px; padding: 0 5px;">×</a></div>`
