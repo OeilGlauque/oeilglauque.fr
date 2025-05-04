6a2127506bf9cde09dfec6a6e2209f50d2050905
ElAurelien <aurelienfoucher@icloud.com>
2025-05-04 12:40:55 +0200

    remove twig components dependencies

--- config/bundles.php
+++ config/bundles.php
@@ -13,6 +13,4 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
-    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
-    Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
 ];
--- importmap.php
+++ importmap.php
@@ -47,7 +47,4 @@ return [
         'version' => '2.4.3',
         'type' => 'css',
     ],
-    '@symfony/ux-live-component' => [
-        'path' => './vendor/symfony/ux-live-component/assets/dist/live_controller.js',
-    ],
 ];
