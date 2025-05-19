d159b6d960585b1710c2a7ea693d36bc7b68a3c0
ElAurelien <aurelienfoucher@icloud.com>
2025-05-19 09:58:22 +0200

    remove search in GameRepository.php

--- src/Repository/GameRepository.php
+++ src/Repository/GameRepository.php
@@ -37,21 +37,6 @@ class GameRepository extends ServiceEntityRepository
             ->getResult();
     }
 
-    public function search(String $query, $validated, Edition $edition): array{
-        return $this->createQueryBuilder('g')
-            ->leftJoin('g.gameSlot', 'gs')
-            ->leftJoin('gs.edition', 'ed')
-            ->where('g.validated = :validated')
-            ->andWhere('ed = :edition')
-            ->andWhere('g.title LIKE :query')
-            ->setParameter('validated', $validated)
-            ->setParameter('edition', $edition)
-            ->setParameter('query', '%' . $query . '%')
-            ->orderBy('g.gameSlot', 'ASC')
-            ->getQuery()
-            ->getResult();
-    }
-
 //    /**
 //     * @return Game[] Returns an array of Game objects
 //     */
