e93999d96634de1fa81c15e111f183264f57d4d4
ElAurelien <aurelienfoucher@icloud.com>
2025-10-07 11:23:20 +0200

    no board game reservations during FOG

--- src/Controller/BoardGameReservationController.php
+++ src/Controller/BoardGameReservationController.php
@@ -38,81 +38,86 @@ class BoardGameReservationController extends FOGController
                 if ($reservation->getDateBeg()->diff($reservation->getDateEnd())->days > 30) {
                     $this->addFlash('warning', "La réservation ne peut pas durer plus d'un mois.");
                 } else {
-                    $overlap = $manager
-                    ->getRepository(BoardGameReservation::class)
-                    ->findBoardGameReservationOverlap($reservation);
-                    
-                    if (count($overlap) != 0) {
-                        $this->addFlash('warning',
-                            "Votre réservation entre en conflit avec une réservation déjà effectuée sur le(s) jeu(x) suivant(s) : "
-                            . implode(', ', $overlap));
-                    } else {
-                        $user = $this->getUser();
-                        $reservation->setAuthor($user);
-        
-                        // Sauvegarde en base
-                        $manager->persist($reservation);
-                        $manager->flush();
+                    $edition = $this->FogParams->getCurrentEdition();
+                    if ($reservation->getDateBeg() < $edition->getEnd() && $reservation->getDateEnd() > $edition->getStart()) {
+                        $this->addFlash('warning', "Pas de réservations possibles pendant la durée du festival !");
+                    }else{
+                        $overlap = $manager
+                            ->getRepository(BoardGameReservation::class)
+                            ->findBoardGameReservationOverlap($reservation);
 
-                        // mail pour l'utilisateur
-                        $mailer->sendTemplatedEmail(
-                            new Address($reservation->getAuthor()->getEmail(), $reservation->getAuthor()->getPseudo()),
-                            'Nouvelle demande de réservation de jeu au FOG',
-                            'oeilglauque/emails/boardGameReservation/nouvelleReservation.html.twig',
-                            ['reservation' => $reservation],
-                            [],
-                            []
-                        );
+                        if (count($overlap) != 0) {
+                            $this->addFlash('warning',
+                                "Votre réservation entre en conflit avec une réservation déjà effectuée sur le(s) jeu(x) suivant(s) : "
+                                . implode(', ', $overlap));
+                        } else {
+                            $user = $this->getUser();
+                            $reservation->setAuthor($user);
 
-                        // mail pour le bureau
-                        $mailer->sendTemplatedEmail(
-                            $mailer->getMailFOG(),
-                            'Nouvelle demande de réservation de jeu au FOG',
-                            'oeilglauque/emails/boardGameReservation/admin/nouvelleReservation.html.twig',
-                            ['reservation' => $reservation],
-                            [],
-                            []
-                        );
+                            // Sauvegarde en base
+                            $manager->persist($reservation);
+                            $manager->flush();
 
-                        // ping discord du bureau
-                        $discord->send(
-                            'Nouvelle demande de réservation de jeux.',
-                            [[
-                                "title" => "Demande de réservation de jeux par " . $reservation->getAuthor()->getPseudo() . " (" . $reservation->getAuthor()->getEmail() . ") du " . $reservation->getDateBeg()->format('d/m/Y') . " au " . $reservation->getDateEnd()->format("d/m/Y"),
-                                "description" => "Liste des jeux :",
-                                "fields" => array_merge(
-                                    array_map(function ($jeu,$price) {
-                                        return ["name" => "- " . $jeu . " (" . $price . " €)", "value" => 
-                                        (!is_null($jeu->getMissing()) && $jeu->getMissing() !== "" ? "Manquant : " . $jeu->getMissing() . "\n" : "") .
-                                        (!is_null($jeu->getExcess()) && $jeu->getExcess() !== "" ? "En trop : " . $jeu->getExcess() . "\n" : "" ) .
-                                        (!is_null($jeu->getNote()) && $jeu->getNote() !== "" ? "Note : " . $jeu->getNote() : "") 
-                                    ];
-                                    }, 
-                                    $reservation->getBoardGames()->getValues(),
-                                    array_map(function ($el) { return $el->getPrice();}, $reservation->getBoardGames()->getValues())
-                                ),
-                                    [
+                            // mail pour l'utilisateur
+                            $mailer->sendTemplatedEmail(
+                                new Address($reservation->getAuthor()->getEmail(), $reservation->getAuthor()->getPseudo()),
+                                'Nouvelle demande de réservation de jeu au FOG',
+                                'oeilglauque/emails/boardGameReservation/nouvelleReservation.html.twig',
+                                ['reservation' => $reservation],
+                                [],
+                                []
+                            );
+
+                            // mail pour le bureau
+                            $mailer->sendTemplatedEmail(
+                                $mailer->getMailFOG(),
+                                'Nouvelle demande de réservation de jeu au FOG',
+                                'oeilglauque/emails/boardGameReservation/admin/nouvelleReservation.html.twig',
+                                ['reservation' => $reservation],
+                                [],
+                                []
+                            );
+
+                            // ping discord du bureau
+                            $discord->send(
+                                'Nouvelle demande de réservation de jeux.',
+                                [[
+                                    "title" => "Demande de réservation de jeux par " . $reservation->getAuthor()->getPseudo() . " (" . $reservation->getAuthor()->getEmail() . ") du " . $reservation->getDateBeg()->format('d/m/Y') . " au " . $reservation->getDateEnd()->format("d/m/Y"),
+                                    "description" => "Liste des jeux :",
+                                    "fields" => array_merge(
+                                        array_map(function ($jeu,$price) {
+                                            return ["name" => "- " . $jeu . " (" . $price . " €)", "value" =>
+                                                (!is_null($jeu->getMissing()) && $jeu->getMissing() !== "" ? "Manquant : " . $jeu->getMissing() . "\n" : "") .
+                                                (!is_null($jeu->getExcess()) && $jeu->getExcess() !== "" ? "En trop : " . $jeu->getExcess() . "\n" : "" ) .
+                                                (!is_null($jeu->getNote()) && $jeu->getNote() !== "" ? "Note : " . $jeu->getNote() : "")
+                                            ];
+                                        },
+                                            $reservation->getBoardGames()->getValues(),
+                                            array_map(function ($el) { return $el->getPrice();}, $reservation->getBoardGames()->getValues())
+                                        ),
                                         [
-                                            "name" => "", "value" => ""
-                                        ],
-                                        [
-                                            "name" => "Note :", "value" => $reservation->getNote()
-                                        ],
-                                        [
-                                            "name" => "Caution totale : " . $reservation->deposit() . " €",
-                                            "value" => ""
-                                        ],
-                                        [
-                                            "name" => "Lien pour gérer la réservation :", "value" => "https://oeilglauque.fr/admin/reservations/boardGame"
+                                            [
+                                                "name" => "", "value" => ""
+                                            ],
+                                            [
+                                                "name" => "Note :", "value" => $reservation->getNote()
+                                            ],
+                                            [
+                                                "name" => "Caution totale : " . $reservation->deposit() . " €",
+                                                "value" => ""
+                                            ],
+                                            [
+                                                "name" => "Lien pour gérer la réservation :", "value" => "https://oeilglauque.fr/admin/reservations/boardGame"
+                                            ]
                                         ]
-                                    ]
-                                )
-                            ]]
-                        );
-        
-                        $this->addFlash('info', "Votre réservation a bien été enregistrée, vous recevrez une confirmation par e-mail dès qu'elle sera acceptée.");
-        
-                        return $this->redirectToRoute('boardGameReservation');
+                                    )
+                                ]]
+                            );
+
+                            $this->addFlash('info', "Votre réservation a bien été enregistrée, vous recevrez une confirmation par e-mail dès qu'elle sera acceptée.");
+
+                            return $this->redirectToRoute('boardGameReservation');
+                        }
                     }
                 }
             }
